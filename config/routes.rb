b996df0701762679404cb8d0327259eeebbd3bd4
ThieuHoai <blne170604@gmail.com>
2024-11-01 00:33:24 +0700

    Add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   authenticate :user, ->(u) { u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
@@ -13,4 +14,9 @@ Rails.application.routes.draw do
   root "home#index"
 
   resources :appoinments
+  resources :visitors
+  resources :payments
+  resources :animal_placements
+  resources :animals
+  resources :categories
 end
