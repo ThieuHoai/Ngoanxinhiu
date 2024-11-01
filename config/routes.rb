0a831cb7727a79134aff34028a81736bab153e06
ThieuHoai <blne170604@gmail.com>
2024-11-01 08:06:48 +0700

    Polishing plate boiler content

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "appoiments#index"
 
   resources :appoinments
   resources :visitors
